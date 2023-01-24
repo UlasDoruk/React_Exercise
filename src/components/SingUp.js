e8f954e636fef5636e490f02786c119eeff7a125
UlaşDK <udk97@hotmail.com>
2023-01-25 01:31:36 +0300

    formik validations

--- src/components/SingUp.js
+++ src/components/SingUp.js
@@ -4,6 +4,7 @@ import "../App.css"
 
 
 function SingUp() {
+
   return (
     <div>
       <h1>Sign Up</h1>
@@ -18,24 +19,43 @@ function SingUp() {
           console.log(values);
         }}
       >
-        {({ handleChange, handleSubmit, values }) => (
+        {({
+          handleChange,
+          handleSubmit,
+          value,
+          errors,
+          handleBlur,
+          touched,
+        }) => (
           <form onSubmit={handleSubmit}>
             <label>Email </label>
-            <input name="email" onChange={handleChange}></input>
+            <input
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+            ></input>
+
+            {errors.email && touched.email && <p>{errors.email}</p>}
 
             <label>Password </label>
             <input
               name="password"
               type="password"
               onChange={handleChange}
+              onBlur={handleBlur}
             ></input>
+
+            {errors.password && touched.password && <p>{errors.password}</p>}
 
             <label>Confirm Password </label>
             <input
               name="cpassword"
               type="password"
               onChange={handleChange}
+              onBlur={handleBlur}
             ></input>
+
+            {errors.cpassword && touched.cpassword && <p>{errors.cpassword}</p>}
 
             <button type="submit">Submit</button>
 
