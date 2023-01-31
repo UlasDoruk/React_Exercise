cc185404b4b4d709e3d08930ea071097ed5d4285
UlaşDK <udk97@hotmail.com>
2023-01-31 19:01:39 +0300

    Some updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css"
+import {  useEffect } from "react";
+import {init} from "./socketApi"
+import SingUp from "./components/FormikYup/SingUp";
 
 function App() {
 
+  useEffect(() => {
+    init()
+  }, []);
+
   return (
     <div className="App">
+      <SingUp/>
     </div>
   );
 }
